0d4a4785afd624ba0894bdcd955086c10b8cd9dc
Zachary Gorak <zac@gorak.us>
2014-01-12 23:59:46 -0500

    works but only on springboard

--- spotlightanywhere.mm
+++ spotlightanywhere.mm
@@ -1,6 +1,7 @@
 #import "FSSwitchDataSource.h"
 #import "FSSwitchPanel.h"
 #import "substrate.h"
+#import <UIKit/UIKit.h>
 
 static BOOL isDisabled;
 
@@ -10,7 +11,19 @@ static BOOL isDisabled;
 @interface SBSearchViewController
 +(id)sharedInstance;
 -(void)searchGesture:(id)arg1 changedPercentComplete:(float)arg2 ;
+-(id)_searchHeader;
 @end
+
+@interface SBSearchGesture
++(id)sharedInstance;
+-(void)setEnabled:(BOOL)arg1 ;
+-(void)revealAnimated:(BOOL)arg1 ;
+-(void)resetAnimated:(BOOL)arg1 ;
+-(id)targetView;
+-(void)setTargetView:(id)arg1 ;
+- (UIViewController*) topMostController;
+@end
+
 
 @implementation FlipSLSwitch
 
@@ -19,24 +32,45 @@ static BOOL isDisabled;
 }
 
 -(void)applyState:(FSSwitchState)newState forSwitchIdentifier:(NSString *)switchIdentifier{
+        SBSearchViewController *vcont = [objc_getClass("SBSearchViewController") sharedInstance];
+        SBSearchGesture *ges = [objc_getClass("SBSearchGesture") sharedInstance];
+
+
+
         if(newState == FSSwitchStateIndeterminate)
                 return;
 
         else if(newState == FSSwitchStateOn){
                 isDisabled = NO;
 
-                SBSearchViewController *vcont = [objc_getClass("SBSearchViewController") sharedInstance];
+                
                 NSLog(@"new state = on");
-                [vcont searchGesture:Nil changedPercentComplete:1.0];
+                //[ges setEnabled:TRUE];
+                [ges revealAnimated:TRUE];
+
+                //[[[UIApplication sharedApplication] keyWindow] addSubview:vcont._searchHeader];
+                //[[UIApplication sharedApplication] bringSubviewToFront:vcont._searchHeader];
+
+                //for (int i = 0.0; i > 1.0; i = i + .1) {
+                //	[vcont searchGesture:ges changedPercentComplete:i];
+                //}
                 
         }
 
         else if(newState == FSSwitchStateOff){
                 isDisabled = YES;
-                SBSearchViewController *vcont = [objc_getClass("SBSearchViewController") sharedInstance];
+                
                 NSLog(@"new state = off");
-                [vcont searchGesture:Nil changedPercentComplete:0.0];
+
+                
+                [ges resetAnimated:TRUE];
+                for (int i = 1.0; i < 0; i = i - .1) {
+                	[vcont searchGesture:ges changedPercentComplete:i];
+                }
+                
         }
 }
+
+
 
 @end
