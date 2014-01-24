0ab9cfd6ecaa8c9d87244575f1b289a1e0ac1400
Zachary Gorak <zac@gorak.us>
2014-01-23 21:57:52 -0500

    view appears and then disapears. help

--- spotlightanywhere.mm
+++ spotlightanywhere.mm
@@ -13,17 +13,7 @@
 
 @end
 
-@interface SBSearchGesture
-+(id)sharedInstance;
--(void)setEnabled:(BOOL)arg1 ;
--(void)revealAnimated:(BOOL)arg1 ;
--(void)resetAnimated:(BOOL)arg1 ;
--(id)targetView;
--(void)setTargetView:(id)arg1 ;
-- (UIViewController*) topMostController;
-@end
-
-@interface SBSearchHeader
+@interface SBSearchHeader : UIView
 @end
 
 @interface SBApplication
@@ -41,13 +31,11 @@
         return [vcont isVisible]?FSSwitchStateOn:FSSwitchStateOff;
 }
 
-static UIWindow *window; 
-
 -(void)applyState:(FSSwitchState)newState forSwitchIdentifier:(NSString *)switchIdentifier{
         SBSearchViewController *vcont = [objc_getClass("SBSearchViewController") sharedInstance];
-        SBSearchGesture *ges = [objc_getClass("SBSearchGesture") sharedInstance];
-
-
+        SBSearchHeader *sheader = MSHookIvar<SBSearchHeader *>(vcont, "_searchHeader");
+        UIView *container = MSHookIvar<UIView *>(sheader, "_container");
+        UITextField *search = MSHookIvar<UITextField *>(sheader, "_searchField");
 
         if(newState == FSSwitchStateIndeterminate)
                 return;
@@ -55,29 +43,75 @@ static UIWindow *window;
         else if(newState == FSSwitchStateOn){
                 
                 NSLog(@"new state = on");
-                UIView *sheader = MSHookIvar<UIView *>(vcont, "_searchHeader");
 
-                [[[UIApplication sharedApplication] keyWindow] insertSubview:sheader atIndex:0];
+                [[[UIApplication sharedApplication] keyWindow] addSubview:sheader];
+                [[[UIApplication sharedApplication] keyWindow] bringSubviewToFront:sheader];
 
-                UIWindow *window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-                window.windowLevel = 9999*999;
-                window.hidden = NO;
-                [window addSubview:MSHookIvar<UIView *>(vcont, "_touchStealingView")];
-				[window addSubview:sheader];
+                //[[[UIApplication sharedApplication] keyWindow] insertSubview:sheader atIndex:0];
+
+                //UIView *container = MSHookIvar<UIView *>(sheader, "_container"); 
+                CGPoint newCenter = sheader.center;
+				newCenter.x = 160;
+				CGRect newBounds = CGRectMake(0, 0, 320, 73);
+
+				sheader.hidden = NO;
+				[sheader setAlpha:1.0];
+
+                [UIView animateWithDuration:1.0 
+			    animations:^{
+			        sheader.center = newCenter;
+			        sheader.bounds = newBounds;
+			    }];
+
+			    newCenter = container.center;
+				newCenter.x = 160;
+				newBounds = CGRectMake(0, 0, 320, 29);
+				CGRect newFrame = CGRectMake(0,31,320,29);
+
+				container.hidden = NO;
+				[container setAlpha:1.0];
+
+                [UIView animateWithDuration:1.0 
+			    animations:^{
+			        container.center = newCenter;
+			        container.bounds = newBounds;
+			        container.frame = newFrame;
+			    }];
+
+				newCenter = search.center;
+				newCenter.x = 127.5;
+				newBounds = CGRectMake(0, 0, 239, 29);
+				newFrame = CGRectMake(8,0,239,29);
+
+				container.hidden = NO;
+				[container setAlpha:1.0];
+
+                [UIView animateWithDuration:1.0 
+			    animations:^{
+			        search.center = newCenter;
+			        search.bounds = newBounds;
+			        search.frame = newFrame;
+			    }];
+
+    //             UIWindow *window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    //             window.windowLevel = 9999*999;
+    //             window.hidden = NO;
+    //             [window addSubview:MSHookIvar<UIView *>(vcont, "_touchStealingView")];
+				// [window addSubview:sheader];
                 // Above displays it but then makes the device unusable (window not dismised)
                 // Also, header doesn't display fully http://i.imgur.com/hwLkw8u.png
 
                 
-                [ges revealAnimated:TRUE];
-                ///[search becomeFirstResponder]; //set cursor location
+                //[ges revealAnimated:TRUE];
+                
+                [search becomeFirstResponder]; //set cursor location
 
                 // http://blog.adambell.ca/post/73338421921/breaking-chat-heads-out-of-the-ios-sandbox
                 // //http://pastie.org/pastes/7618709#2
                 // UIView *hostView = [app1 contextHostViewForRequester:@"epichax1" enableAndOrderFront:YES];
-                //UITextField *search = MSHookIvar<UITextField *>(sheader, "_searchField");
+                //
                 
-                // [[[UIApplication sharedApplication] keyWindow] addSubview:sheader];
-                // [[[UIApplication sharedApplication] keyWindow] bringSubviewToFront:sheader];
+                
 
                 
                 //SBApplication *currentApplication = [objc_getClass("SpringBoard-Class") _accessibilityFrontMostApplication];
@@ -95,7 +129,7 @@ static UIWindow *window;
                 
                 NSLog(@"new state = off");
 
-                [ges resetAnimated:TRUE];
+                //[ges resetAnimated:TRUE];
                 
         }
 }
